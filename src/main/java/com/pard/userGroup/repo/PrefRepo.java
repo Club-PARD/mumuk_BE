2ba4b769496b8b3ed37feebf85e6c8e2b3fad7eb
SionLee21 <sionl1030@gmail.com>
2024-06-29 20:49:09 +0900

    임시 group기능추가 버전

--- src/main/java/com/pard/userGroup/repo/PrefRepo.java
+++
@@ -1,7 +0,0 @@
-package com.pard.userGroup.repo;
-
-import com.pard.preferences.entity.Preferences;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrefRepo extends JpaRepository<Preferences, Long> {
-}
