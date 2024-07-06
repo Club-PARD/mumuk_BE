93ac356db6a6a022bcb044232f2a58e77debe4ec
SionLee21 <sionl1030@gmail.com>
2024-07-06 18:04:48 +0900

    foodList 링크 추가

--- src/main/java/com/pard/food/service/FoodListService.java
+++ src/main/java/com/pard/food/service/FoodListService.java
@@ -4,16 +4,18 @@ import com.pard.food.dto.FoodListDto;
 import com.pard.food.entity.FoodList;
 import com.pard.food.repo.FoodListRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FoodListService {
     private final FoodListRepo foodListRepo;
+
+    @Value("${aws.s3}")
+    private String s3BaseUrl;
 
     public List<FoodListDto> getAllFoodNames() {
         return foodListRepo.findAllFoodNames();
@@ -22,8 +24,8 @@ public class FoodListService {
     public FoodListDto.Read getFoodWithUrl(String foodName) {
         FoodList food = foodListRepo.findByFoodName(foodName)
                 .orElseThrow(() -> new RuntimeException("Food not found: " + foodName));
-        // S3 링크 생성
-        String s3Link = "https://mumukbucket.s3.ap-northeast-2.amazonaws.com/foods/" + food.getS3Link();
+
+        String s3Link = s3BaseUrl + food.getS3Link();
         return new FoodListDto.Read(food.getFoodName(), s3Link);
     }
 }
